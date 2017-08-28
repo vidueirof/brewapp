2e0106ef3f5d9032715aab5cfbb75cf4caac9bb4
Federico Vidueiro <vidueiro@eudaimonia.com.ar>
2017-08-28 16:02:42 -0300

    Fix scripts

--- scripts/package-and-upload.sh
+++ scripts/package-and-upload.sh
@@ -9,9 +9,7 @@ fi
 #####################
 OUTPUTDIR="$PWD/platforms/ios/build/device"
 
-xcrun -log -sdk iphoneos \  
-PackageApplication -v "$OUTPUTDIR/$APP_NAME.app" \  
--o "$OUTPUTDIR/$APP_NAME.ipa"
+xcrun -log -sdk iphoneos PackageApplication -v "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa"
 
 /usr/bin/zip --verbose --recurse-paths "$OUTPUTDIR/$APP_NAME.dsym.zip" "$OUTPUTDIR/$APP_NAME.app.dsym"
 
@@ -30,14 +28,7 @@ if [[ "$TRAVIS_BRANCH" == "$HOCKEYAPP_UPLOAD_BRANCH" ]]; then
   fi
 
   echo "At $HOCKEYAPP_UPLOAD_BRANCH branch, upload to hockeyapp."
-  curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload \
-    -F status="2" \
-    -F notify="0" \
-    -F notes="$DELIVER_WHAT_TO_TEST" \
-    -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
-    -F dsym="@$OUTPUTDIR/$APP_NAME.dsym.zip" \
-    -F commit_sha="$TRAVIS_COMMIT" \
-    -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+  curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload -F status="2" -F notify="0" -F notes="$DELIVER_WHAT_TO_TEST" -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" -F dsym="@$OUTPUTDIR/$APP_NAME.dsym.zip" -F commit_sha="$TRAVIS_COMMIT" -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
 
   if [[ $? -ne 0 ]]; then
     echo "Error: Fail uploading to HockeyApp"
